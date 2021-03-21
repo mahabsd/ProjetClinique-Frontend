d5da10931c5da0f4d8497fa244b429b359e15628
Mahabsd <Mahabsd@gmail.com>
2021-03-21 22:55:59 +0100

    ws to wss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   basUrl :  "https://okbaclinical-backend.herokuapp.com/api/",
-  socketBaseUrl : "ws://okbaclinical-backend.herokuapp.com"
+  socketBaseUrl : "wss://okbaclinical-backend.herokuapp.com"
 };
